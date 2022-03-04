15342e5d4ba23d416ca521bad0505a2113316259
Shubham Saurabh <shubhamsaurabh@outlook.com>
2022-03-05 01:47:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+
+  - [Built with](#built-with)
+
+- [Author](#author)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [See Solution](https://your-solution-url.com)
+- Live Site URL: [Live Site](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [@shubhsaur](https://www.frontendmentor.io/profile/shubhsaur)
+- Twitter - [@shubh_saur](https://www.twitter.com/shubh_saur)
---
+++ script.js
@@ -0,0 +1,79 @@
+const billInput = document.querySelector(".bill-input");
+const peopleInput = document.querySelector(".person-input");
+const tipPerPerson = document.querySelector(".tip-amount-value");
+const billPerPerson = document.querySelector(".bill-amount-value");
+const tips = document.querySelectorAll(".tip");
+const tipCustom = document.querySelector(".tip-custom");
+const resetButton = document.querySelector(".reset");
+const errorText = document.querySelector(".error-text");
+
+billInput.addEventListener("input", billInputFunction);
+peopleInput.addEventListener("input", peopleInputFunction);
+tips.forEach(function (val) {
+	val.addEventListener("click", handleClick);
+});
+tipCustom.addEventListener("input", tipCustomFunction);
+resetButton.addEventListener("click", reset);
+
+billInput.value = "0.0";
+peopleInput.value = "1";
+tipPerPerson.innerHTML = `&#36;${(0.0).toFixed(2)}`;
+billPerPerson.innerHTML = `&#36;${(0.0).toFixed(2)}`;
+
+let billValue = "0.0";
+let peopleValue = "1";
+let tipValue = 0.15;
+
+function billInputFunction() {
+	billValue = parseFloat(billInput.value);
+	calculateTip();
+}
+
+function peopleInputFunction() {
+	peopleValue = parseFloat(peopleInput.value);
+	calculateTip();
+
+	if (peopleValue < 1) {
+		errorText.style.display = "block";
+		peopleInput.classList.add("error-border");
+	} else {
+		errorText.style.display = "none";
+		peopleInput.classList.remove("error-border");
+	}
+}
+
+function handleClick(event) {
+	tips.forEach(function (val) {
+		val.classList.remove("active-tip");
+		if (event.target.innerHTML == val.innerHTML) {
+			val.classList.add("active-tip");
+			tipValue = parseFloat(val.innerHTML) / 100;
+		}
+	});
+	calculateTip();
+}
+
+function calculateTip() {
+	if (peopleValue >= 1) {
+		let tipAmount = (billValue * tipValue) / peopleValue;
+		let total = (billValue + tipAmount) / peopleValue;
+		tipPerPerson.innerHTML = `&#36;${tipAmount.toFixed(2)}`;
+		billPerPerson.innerHTML = `&#36;${total.toFixed(2)}`;
+	}
+}
+
+function tipCustomFunction() {
+	tipValue = parseFloat(tipCustom.value / 100);
+	tips.forEach(function (val) {
+		val.classList.remove("active-tip");
+	});
+	calculateTip();
+}
+
+function reset() {
+	billInput.value = "0.0";
+	peopleInput.value = "1";
+	tipCustom.value = "";
+	tipPerPerson.innerHTML = `&#36;${(0.0).toFixed(2)}`;
+	billPerPerson.innerHTML = `&#36;${(0.0).toFixed(2)}`;
+}
